9368cb8e3b995611dc2e43cbed1b239964e8afc7
MarceloMenoli <marcelomenoli7@gmail.com>
2023-02-26 00:59:00 -0300

    first commit

---
+++ src/components/App/index.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+// import reactLogo from './assets/react.svg'
+import reactLogo from "../../assets/react.svg";
+import { Home } from "../../pages/Home";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
